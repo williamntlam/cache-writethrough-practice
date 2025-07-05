76b03461f38307eafd87eeffd3850688a0938c83
William Lam <williamntlam@gmail.com>
2025-07-05 17:18:47 -0400

    feat: created redis client connection

--- redis/redis.go
+++ redis/redis.go
@@ -2,9 +2,33 @@ import (
     "context"
     "fmt"
     "github.com/redis/go-redis/v9"
+	"github.com/joho/godotenv"
 )
 
+var RedisDatabase *redis.Client
 
-func main() {
+func connectToRedis() {
+
+	godotenv.Load()
+	host := os.Getenv("REDIS_HOST")
+	port := os.Getenv("REDIS_PORT")
+
+	RedisDatabase = redis.NewClient(&redis.Options{
+
+		Addr:     fmt.Sprintf("%s:%s", host, port),
+        Password: "", // or os.Getenv("REDIS_PASSWORD")
+        DB:       0,  // default DB
+
+	})
+
+	_, error = RedisDatabase.Ping(context.Background()).Result()
+
+	if error != nil {
+
+		panic("Failed to connect to Redis: " + error.Error())
+
+	}
+
+	fmt.Println("Connected to Redis")
 
 }
