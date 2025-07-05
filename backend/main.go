240f18b8c0cd672dae08b1b04230c2b7af33fc03
William Lam <williamntlam@gmail.com>
2025-07-05 17:06:52 -0400

    feat: added context library in backend

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,20 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
 type Todo struct {
 	ID string `json:"id"`
 	Title string `json:"title`
 }
 
+var ctx = context.Background()
+
 func main() {
-	
+
+
+
 }
