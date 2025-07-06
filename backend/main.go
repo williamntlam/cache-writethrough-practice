a088d820ee8f2ed38804c913f13592f90f41c91e
William Lam <williamntlam@gmail.com>
2025-07-06 12:58:10 -0400

    feat: added error handling for database / redis cache connection error

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"github.com/gin-gonic/gin"
-	// "github.com/redis/go-redis/v9"
-	// "fmt"
-	// "net/http"
-	// "cache-writethrough-practice/postgres"
-	// "cache-writethrough-practice/redis"
+	"cache-writethrough-practice/postgres"
+	"cache-writethrough-practice/redis"
 )
 
 var ctx = context.Background()
@@ -15,6 +14,26 @@ var ctx = context.Background()
 func main() {
 
 	router := gin.Default()
+
+	db, err := postgres.ConnectToPostgres()
+
+	if err != nil {
+		log.Fatal("Failed to connect to PostgreSQL:", err)
+		os.Exit(1);
+	} 
+
+	defer db.Close()
+	fmt.Println("Connected to Postgres");
+
+	redisClient, err := redis.ConnectToRedis()
+
+	if err != nil {
+		log.Fatal("Failed to connect to Redis:", err)
+		os.Exit(1);
+	}
+
+	defer redisClient.Close()
+	fmt.Println("Connected to Redis")
 
 	// POST - create a task
 	// GET - get a task
